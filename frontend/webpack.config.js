f2b05424b5dbf88a0073144bb666431391f96dc7
Alan Vasconcelos <alan-vasconcelos10@hotmail.com>
2018-11-22 14:12:59 -0300

    init front

---
+++ frontend/webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve('public'),
+    filename: 'app.js'
+  },
+  devServer: {
+    port: 8000,
+    contentBase: './public'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      modules: path.join(__dirname, '/node_modules')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /.js[x]?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['react']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  }
+}
